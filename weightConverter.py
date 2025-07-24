95dc003f6e70a6fd728d7cfbc846a8cdb63fae04
fathimaaa20 <fathimahabeebb@gmail.com>
2025-07-24 12:05:11 +0530

    weight

--- weightConverter.py
+++ weightConverter.py
@@ -1,3 +1,12 @@
-#python weight converter
-weight = float(input("Enter weight:"))
-unit = input("Kg or Pound ():")
+# Python Weight Converter
+weight = float(input("Enter weight: "))
+unit = input("Kg or Pound? (k or l): ")
+
+if unit.lower() == 'k':
+    converted_weight = weight * 2.205
+    print(f"{weight} kg is equal to {converted_weight:.2f} pounds.")
+elif unit.lower() == 'l':
+    converted_weight = weight / 2.205
+    print(f"{weight} pounds is equal to {converted_weight:.2f} kg.")
+else:
+    print("Invalid unit. Please enter 'k' for kg or 'l' for pounds.")
